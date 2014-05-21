16b2e7f271dea26f4b51873c921286b1dc62d3f8
root (Jay) <jay@mgmt.sqrawler.com>
2014-05-22 11:54:35 +1200

    fix apache mod in apache_owncloud config manifest

--- puppet/modules/apache_owncloud/manifests/config.pp
+++ puppet/modules/apache_owncloud/manifests/config.pp
@@ -12,27 +12,30 @@
 #
 class apache_owncloud::config {
       
-      # push 000-default to client
-      file {"/etc/apache2/sites-enabled/000-default":
-              ensure    => present,
-              owner     => "root" ,
-              group     => "root",
-              mode      => 0644,
-              source    => "puppet:///modules/apache_owncloud/000-default",
-              require   => Class["apache_owncloud::install"],
+      	# push 000-default to client
+      	file {"/etc/apache2/sites-enabled/000-default":
+              ensure    	=> present,
+              owner     	=> "root" ,
+              group     	=> "root",
+              mode      	=> 0644,
+              source    	=> "puppet:///modules/apache_owncloud/000-default",
+              require   	=> Class["apache_owncloud::install"],
               
-      }
+      	}
 
-      # install apache modules: a2enmod rewrite and a2enmod headers then restart service
-      define apache_owncloud::loadmodule () {
-                             
-          exec { "/usr/sbin/a2enmod $name" :
-                unless    => "/bin/readlink -e /etc/apache2/mods-enabled/${name}.load",
-                notify    => Class["apache_owncloud::service"],
-          }
-      }
-            
-      apache_owncloud::loadmodule{"rewrite": }
-      apache_owncloud::loadmodule{"headers": }
+	# create symlink to apache headers module
+	file { "/etc/apache2/mods-enabled/headers.load":
+		ensure 		=> "link",
+		mode      	=> 0777,
+		target 		=> "/etc/apache2/mods-available/headers.load",
+	}
+
+	# create symlink to apache rewrite module
+	file { "/etc/apache2/mods-enabled/rewrite.load":
+		ensure 		=> "link",
+		mode            => 0777,
+		target 		=> "/etc/apache2/mods-available/rewrite.load",
+		notify    	=> Class["apache_owncloud::service"],
+	}
       
 }
