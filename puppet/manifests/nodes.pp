f24fa3aeb518d131d9650a4bd8d4d3f94f9ab053
root (Jay) <jay@mgmt.sqrawler.com>
2014-05-25 21:13:25 +1200

    add puppet users module

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -4,11 +4,13 @@ node 'mgmt'{
 	include mc
 	include nagios
 	include nix_bacula_client
-	include sudo
 	include hosts_file
+	include sudo
+	include users	
 }
 
 node 'db'{
+	include users
 	package { 'vim': ensure => present }
 	package { 'tree': ensure => present }
 	include sudo	
@@ -21,6 +23,7 @@ node 'db'{
 }
 
 node 'app'{
+	include users
         package { 'vim': ensure => present }
         package { 'tree': ensure => present }
 	include sudo
@@ -28,9 +31,12 @@ node 'app'{
 	include nagios-nrpe-server
 	include mc
 	include nix_bacula_client
+	include apache_owncloud
+#	include owncloud
 }
 
 node 'backup'{
+	include users
         package { 'vim': ensure => present }
         package { 'tree': ensure => present }
 	include sudo
---
+++ puppet/modules/users/manifests/init.pp
@@ -0,0 +1,18 @@
+class users {
+                user { 'steeljm1':
+                        ensure     => 'present',
+                        managehome => true,
+                        password        => '$6$hpf9cHH8$PgXqxmqqetlqkV/AmrEWiXDiRMzmaSwkGZJz6.aBeEAFVnzeK4.RybizSPZInBWWeAYNSB8uNKTBLRpcujnHg/',
+                        shell => '/bin/bash',
+                }
+
+               # user { 'unverzp1':
+               #         ensure     => 'present',
+               #         managehome => true,
+               #         password        => '$6$hpf9cHH8$PgXqxmqqetlqkV/AmrEWiXDiRMzmaSwkGZJz6.aBeEAFVnzeK4.RybizSPZInBWWeAYNSB8uNKTBLRpcujnHg/',
+               #         shell => '/bin/bash',
+               # }
+                
+                
+}
+
