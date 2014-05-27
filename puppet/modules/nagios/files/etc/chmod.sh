1beb6996cf8ad6d34d2356d6631b875e1e8c654d
root (Jay) <jay@mgmt.sqrawler.com>
2014-05-27 18:25:12 +1200

    add comments to chmod.sh

--- puppet/modules/nagios/files/etc/chmod.sh
+++ puppet/modules/nagios/files/etc/chmod.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Chmod script to change all files in /etc/nagios3/conf.d/ to 0644
+# Written by J.Steele
+#
+# NOTE: Do NOT change mode or nagios3 will NOT start
+#
 
 cd /etc/nagios3/conf.d/
 
